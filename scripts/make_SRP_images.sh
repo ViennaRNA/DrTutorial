a6deabdd2547da3a45fbcbc194932867faa4f625
Ronny Lorenz <ronny@tbi.univie.ac.at>
2022-11-16 13:18:34 +0100

    Move motif plot scripts and input to DrTutorial-data directory

---
+++ scripts/make_SRP_images.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+#
+
+SEQUENCE_DIR="sequences"
+BACKBONE_COL="0.7 0.6 0.6"
+PAIR_COL="0.9 0.6 0.4"
+NATIVE_STEM_COL="0.4 0.6 0.4"
+NATIVE_COL="0.7 0.9 0.7"
+ANNOT_COL="0. 0.3 0.6"
+POS_COL="0.5 0.5 0.5"
+LABEL_FONT="0.1 0.3 0.1 setrgbcolor (Times) 2 LabelFont"
+
+ANNOT_H1_STEM="3 25 ${NATIVE_COL} Fomark 4 24 8 20 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H1A_STEM="4 22 ${NATIVE_COL} Fomark 4 22 8 18 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H1B_STEM="3 38 ${NATIVE_COL} Fomark 3 38 7 34 ${NATIVE_STEM_COL} BFmark 8 31 9 30 ${NATIVE_STEM_COL} BFmark 12 27 14 25 ${NATIVE_STEM_COL} BFmark 16 23 17 22 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H1C_STEM="4 23 ${NATIVE_COL} Fomark 4 23 8 19 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H2_STEM="28 85 41 71 ${NATIVE_COL} BFmark 28 85 31 82 ${NATIVE_STEM_COL} BFmark"
+ANNOT_R1_STEM="35 77 41 71 ${NATIVE_STEM_COL} BFmark 46 69 ${NATIVE_COL} Fomark 46 69 48 67 ${NATIVE_STEM_COL} BFmark 53 62 55 60 ${NATIVE_STEM_COL} BFmark"
+#ANNOT_H2A_STEM="30 65 ${NATIVE_COL} Fomark 30 65 32 63 ${NATIVE_STEM_COL} BFmark 34 60 38 55 ${NATIVE_STEM_COL} BFmark 42 53 44 51 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H2A_STEM="36 57 ${NATIVE_COL} Fomark 36 57 38 55 ${NATIVE_STEM_COL} BFmark 42 53 44 51 ${NATIVE_STEM_COL} BFmark"
+#ANNOT_H3A_STEM="68 93 ${NATIVE_COL} Fomark 68 93 69 92 ${NATIVE_STEM_COL} BFmark 72 90 75 87 ${NATIVE_STEM_COL} BFmark 77 85 79 83 ${NATIVE_STEM_COL} BFmark"
+ANNOT_H3A_STEM="72 90 ${NATIVE_COL} Fomark 72 90 75 87 ${NATIVE_STEM_COL} BFmark 77 85 79 83 ${NATIVE_STEM_COL} BFmark"
+
+ANNOT_SETTINGS="${ANNOT_COL} setrgbcolor 2 setlinewidth"
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1_STEM} gsave ${ANNOT_SETTINGS} 21 cmark 21 1 -0.4 (U21C) Label 22 cmark 22 1 -0.4 (C22U) Label ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 0.5 -0.4 (20) Label 1 -1.5 -0.4 (5') Label 26 1 -0.4 (3') Label ${LABEL_FONT} 6 -3.5 -0.5 (H1) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1.fa
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1A_STEM} gsave ${ANNOT_SETTINGS} 21 cmark 21 1 -0.4 (U21C) Label 22 cmark 22 -1 -1.5 (C22U) Label ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 0.5 -0.4 (20) Label 1 -1.5 -0.4 (5') Label 26 1 -0.4 (3') Label ${LABEL_FONT} 6 -4.5 -0.5 (H1a) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1a.fa
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1B_STEM} gsave ${ANNOT_SETTINGS} 35 cmark 37 cmark 35 1 -0.4 (U35C) Label 37 1 -0.4 (U37C) Label ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -0.5 -1 (10) Label 20 -0.5 0.5 (20) Label 30 0.5 0 (30) Label 1 -1.5 -0.4 (5') Label 41 1 -0.4 (3') Label ${LABEL_FONT} 11 -4.5 -0.5 (H1b) Label  grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1b.fa
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1C_STEM} gsave ${ANNOT_SETTINGS} 21 cmark 21 1 -0.4 (U21C) Label 22 cmark 22 1 -0.4 (C22U) Label ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 0.5 -0.4 (20) Label 1 -1.5 -0.4 (5') Label 26 1 -0.4 (3') Label ${LABEL_FONT} 6 -4.5 -0.5 (H1c) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1c.fa
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1_STEM} ${ANNOT_H2_STEM} ${ANNOT_R1_STEM} 87 105 ${NATIVE_COL} Fomark 87 105 90 102 ${NATIVE_STEM_COL} BFmark gsave ${ANNOT_COL} setrgbcolor 2 setlinewidth 21 cmark 22 cmark 35 cmark 37 cmark 93 cmark ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 0.5 -0.4 (20) Label 30 -1.5 -0.4 (30) Label 40 -1.5 -0.4 (40) Label 50 -1.5 -0.4 (50) Label 60 0.5 -0.5 (60) Label 70 0.5 -0.4 (70) Label 80 0.5 -0.4 (80) Label 90 -1.5 -0.4 (90) Label 100 0.5 -0.4 (100) Label 1 -1.5 -0.4 (5') Label 106 1 -0.4 (3') Label ${LABEL_FONT} 6 -3.5 -0.5 (H1) Label 75 1 -0.5 (R1) Label 50 -4.5 -0.5 (H2) Label 103 1 -0.5 (H3) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1-H2-R1-H3.fa
+
+RNAplot \
+  -t4 \
+  --pre="${ANNOT_H1C_STEM} ${ANNOT_H2A_STEM} ${ANNOT_H3A_STEM} gsave ${ANNOT_COL} setrgbcolor 2 setlinewidth 21 cmark 22 cmark 35 cmark 37 cmark 93 cmark ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 0.5 -0.4 (20) Label 30 -0.4 -1.2 (30) Label 40 -1.3 -0.4 (40) Label 50 0.5 -0.4 (50) Label 60 0.5 -0.1 (60) Label 70 -1.3 -0.4 (70) Label 80 -0.6 0.5 (80) Label 90 0.5 -0.4 (90) Label 1 -1.5 -0.4 (5') Label 95 1 -0.4 (3') Label ${LABEL_FONT} 6 -4.5 -0.5 (H1c) Label 55 1 -0.5 (H2a) Label 88 1 -0.5 (H3a) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H1c-H2a-H3a.fa
+
+RNAplot \
+  -t4 \
+  --pre="4 113 19 98 ${NATIVE_STEM_COL} BFmark ${ANNOT_H2_STEM} ${ANNOT_R1_STEM} gsave ${ANNOT_COL} setrgbcolor 2 setlinewidth 21 cmark 21 -3.5 -0.4 (U21C) Label 22 cmark 22 -3.5 -0.4 (C22U) Label 35 cmark 37 cmark 35 -3.5 -0.4 (U35C) Label 37 -3.5 -0.4 (U37C) Label 93 cmark 93 1 -0.4 (G93A) Label ${POS_COL} setrgbcolor (Helvetica) 0.8 LabelFont 10 -1.5 -0.4 (10) Label 20 -1.5 -0.4 (20) Label 30 -1.5 -0.4 (30) Label 40 -1.5 -0.4 (40) Label 50 -1.5 -0.4 (50) Label 60 0 -1.2 (60) Label 70 0.5 -0.4 (70) Label 80 0.5 -0.4 (80) Label 90 0.5 -0.4 (90) Label 100 0.5 -0.4 (100) Label 110 0.5 -0.4 (110) Label 1 -1.5 -0.4 (5') Label 117 1 -0.4 (3') Label ${LABEL_FONT} 50 -4.5 -0.5 (H2) Label 76 2 -0.5 (R1) Label 106 2 -0.5 (R2) Label grestore" \
+  ${SEQUENCE_DIR}/SRPn-H2-R1-R2.fa
+
+# rename files
+for f in \
+    SRPn-H1_ss.ps \
+    SRPn-H1a_ss.ps \
+    SRPn-H1b_ss.ps \
+    SRPn-H1c_ss.ps \
+    SRPn-H1-H2-R1-H3_ss.ps \
+    SRPn-H1c-H2a-H3a_ss.ps \
+    SRPn-H2-R1-R2_ss.ps
+do
+  # post-process
+  sed -i 's#/fsize  14#/fsize  18#g' $f
+  sed -i 's#/outlinecolor {0.2 setgray}#/outlinecolor { '"${BACKBONE_COL}"' setrgbcolor}#g' $f
+  sed -i 's#/paircolor    {0.2 setgray} bind def#/paircolor     {'"${PAIR_COL}"' setrgbcolor} bind def#g' $f
+  sed -i 's#  0.7 setlinewidth#  5 setlinewidth#g' $f
+  sed -i 's#/seqcolor     {0   setgray}#/seqcolor     {0.3   setgray}#' $f
+
+  # increase plot area
+  sed -i 's#%%BoundingBox: 0 0 700 700#%%BoundingBox: -70 -70 770 770#' $f
+
+#  sed -i 's#^drawoutline#%drawoutline#g' $f
+
+  /usr/bin/mv $f `basename $f _ss.ps`.eps
+done
